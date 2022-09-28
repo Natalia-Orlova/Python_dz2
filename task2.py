51cfa42f0935a729fb9d21d54833344ef27c120c
NataliaOrlova <natakarelskaya@yandex.ru>
2022-09-28 18:37:12 +0400

    task2

--- task2.py
+++ task2.py
@@ -4,3 +4,11 @@
 # 1 - 1 * 1, 2 - 1 * 2, 3 - 1 * 2 * 3, 4 - 1 * 2 * 3 * 4 и т.д.
 # - 4 -> [1, 2, 6, 24]
 # - 6 -> [1, 2, 6, 24, 120, 720]
+
+n = int(input("Enter number: "))
+lis = list()
+f = 1
+for i in range(1, n+1):
+    lis.append(f * i)
+    f = f * i
+print(f"{n} -> {lis}")
