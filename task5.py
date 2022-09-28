b55b522ec0ee8ac7fcb6c34231d2a6704fc0c82f
NataliaOrlova <natakarelskaya@yandex.ru>
2022-09-28 22:42:04 +0400

    task5

--- task5.py
+++ task5.py
@@ -2,3 +2,14 @@
 # 10
 # -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # -> [0, 7, 6, 3, 4, 2, 9, 5, 1, 8]
+
+import random
+n = int(input("Введите число: "))
+list1 = list(range(0, n))
+print(list1)
+list2 = list()
+for i in range(0, n):
+    rand_idx = random.randrange(len(list1))
+    list2.append(list1[rand_idx])
+    list1.remove(list2[i])
+print(list2)
